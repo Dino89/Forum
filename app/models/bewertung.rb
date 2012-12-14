a279946078989ca926aea41e4963e810245abae5
DJ <kleinerlahm@googlemail.com>
2012-12-14 16:15:29 +0100

    Fehler bei Models behoben

--- app/models/bewertung.rb
+++ app/models/bewertung.rb
@@ -2,5 +2,4 @@ class Bewertung < ActiveRecord::Base
   attr_accessible :Author, :KommentarID
   has_one:user_id
   has_one:kommentar_id
-  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([{:Name => "Mario", :Vorname => "Gomez", :Nickname => "SuperMario", :Email => "gomez@fcb.de", :Passwort => "123456", :Berechtigung => false, :Ort => "Muenster", :Geschlecht => "m"},
+  {:Name => "Toni", :Vorname => "Kroos", :Nickname => "TK", :Email => "kroos@fcb.de", :Passwort => "123456", :Berechtigung => false, :Ort => "Muenchen", :Geschlecht => "m"}])
