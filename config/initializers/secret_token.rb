ecb9f9bab0ddf9956addc8e22555b562aaef0761
DJ <kleinerlahm@googlemail.com>
2012-12-21 14:45:09 +0100

    Projekt erstellt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = 'ecc13f8757c3dbad4bd3662325a6a982a899455230f5cbdabc2131c85cfb07603171278af811f01aaf98760309a342b406c8238662dc13b5ba073ec2857fe83b'
