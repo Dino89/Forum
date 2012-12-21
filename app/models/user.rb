2ff19a6276d9bf62cb3168a231a88e663fa675fd
DJ <kleinerlahm@googlemail.com>
2012-12-21 22:04:46 +0100

    right aus getter im Model geloescht

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :firstname, :gender, :id, :location, :name, :nickname, :password, :right, :admin
+  attr_accessible :email, :firstname, :gender, :id, :location, :name, :nickname, :password, :admin
   has_many :comments
   has_many :subscriptions
   has_many :themes
