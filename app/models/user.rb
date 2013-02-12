167696cbe674f3c35cb280c3d2231ff46a973550
Dino <ch.mueller3@live.de>
2013-02-12 12:16:18 +0100

    Devise installiert und Seeds angepasst

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :firstname, :gender, :id, :location, :name, :nickname, :password, :admin
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :firstname, :name, :location, :gender, :admin, :nickname, :encrypted_password
+  # attr_accessible :title, :body
+  
   has_many :comments
   has_many :subscriptions, :dependent => :destroy
   has_many :themes
@@ -8,6 +17,5 @@ class User < ActiveRecord::Base
   validates :nickname, :email, :password, presence: false
   validates :nickname, :email, uniqueness: true
   validates_length_of :password, :minimum => 5, :message => 'Passwort >= 5 Zeichen'
-  #validates :nickname, length: {minimum: 3}
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Forum::Application.routes.draw do
+  devise_for :users
+
   resources :ratings
 
   resources :comments
@@ -8,8 +10,6 @@ Forum::Application.routes.draw do
   resources :rooms
 
   resources :themes
-
-  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
