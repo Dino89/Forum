f34e9f14c32f3f402a758b0b1b9322ef33e1f58f
Dino <ch.mueller3@live.de>
2013-02-13 15:29:27 +0100

    LikeButton funktioniert

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,13 @@
 module CommentsHelper
+  
+  def toggle_like_button(comment, user)
+    if user.flagged?(comment, :like)
+      
+      link_to 'dislike', like_comment_path(comment)
+    else
+      
+      link_to 'like', like_comment_path(comment)
+    end
+  end
+    
 end
