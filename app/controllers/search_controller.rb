39d6194a2c671bc88fe720375f0b9deface78730
Christopher Dicke <hum21@web.de>
2013-02-19 01:37:58 +0100

    Suche + Bug in Themenansicht

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,18 @@
+class SearchController < ApplicationController
+  
+  def index
+      
+      if params[:searchstring] && params[:searchstring] != "" 
+
+        
+     # @theme = Theme.find_by_title(params[:searchstring])  
+      @theme = Theme.find_by_sql(["SELECT * FROM themes where title like ? OR description like ?", "%"+params[:searchstring]+"%","%"+params[:searchstring]+"%"])
+      @comment = Comment.find_by_sql(["SELECT * FROM comments where title like ? OR text like ?", "%"+params[:searchstring]+"%","%"+params[:searchstring]+"%"])
+      respond_to do |format|
+      format.html # index.html.erb
+      #format.json { render json: @rooms }
+      end
+      
+    end
+  end
+end
