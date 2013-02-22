c3442c414f93b3856555a8c01f16676b3f509289
DJ <kleinerlahm@googlemail.com>
2013-02-22 03:33:11 +0100

    column admin in users deleted

--- db/migrate/20130212135520_add_colums_in_table_users.rb
+++ db/migrate/20130212135520_add_colums_in_table_users.rb
@@ -3,7 +3,6 @@ class AddColumsInTableUsers < ActiveRecord::Migration
     add_column(:users, :name, :string)
     add_column(:users, :firstname, :string)
     add_column(:users, :nickname, :string)
-    add_column(:users, :admin, :boolean)
     add_column(:users, :location, :string)
     add_column(:users, :gender, :string)
   end
